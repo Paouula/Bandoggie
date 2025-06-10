9427f34d1c13f2b1c4a5c328168664d5533fbd86
Rony <ronyjavierramirezalvarado@gmail.com>
2025-06-10 15:19:58 -0600

    Diseño preliminar del login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,66 +1,67 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import useFetchLogin from '../hooks/Login/UseFetchLogin';
-import '../assets/style/style.css';
-import InputComponent from '../components/Input';
-import ButtonComponent from '../components/Button';
+import useFetchLogin from "../hooks/Login/UseFetchLogin";
+import "../assets/style/style.css";
+import InputComponent from "../components/Input";
+import ButtonComponent from "../components/Button";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const { register, handleSubmit, reset } = useForm();
-    const { handleLogin } = useFetchLogin();
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
+  const { handleLogin } = useFetchLogin();
 
-    const onSubmit = async (data) => {
-        try {
-            const response = await handleLogin(data.email, data.password);
-            if (response) {
-                toast.success("Login successful");
-                reset();
-                navigate('/dashboard');
-            }
-        } catch (error) {
-            toast.error("Login failed. Please check your credentials.");
-        }
-    };
+  const onSubmit = async (data) => {
+    try {
+      const response = await handleLogin(data.email, data.password);
+      if (response) {
+        toast.success("Login successful");
+        reset();
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      toast.error("Login failed. Please check your credentials.");
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <div className="login-box">
-                <div className="logo">
-                    <img src="https://i.imgur.com/1Xi0X2z.png" alt="HUELLITAS Logo" />
-                    <p>pet - shop</p>
-                </div>
-                <hr />
-                <h2>Iniciar Sesión</h2>
-                <p className="small-link">¿No tienes una cuenta aún?</p>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label htmlFor="email">Correo Electrónico</label>
-                    <InputComponent
-                        type="email"
-                        id="email"
-                        placeholder="Ingresa tu correo"
-                        register={register('email', { required: true })}
-                        icon="📧"
-                    />
-
-                    <label htmlFor="password">Contraseña</label>
-                    <InputComponent
-                        type="password"
-                        id="password"
-                        placeholder="Ingresa tu contraseña"
-                        register={register('password', { required: true })}
-                        icon="👁️"
-                    />
-
-                    <p className="forgot">¿Olvidaste tu contraseña?</p>
-
-                    <ButtonComponent type="submit">Confirmar</ButtonComponent>
-                </form>
-            </div>
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="logo">
+          <img src="https://i.imgur.com/1Xi0X2z.png" alt="HUELLITAS Logo" />
+          <p>pet - shop</p>
         </div>
-    );
+        <hr />
+        <h2>Iniciar Sesión</h2>
+        <p className="small-link">¿No tienes una cuenta aún?</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email">Correo Electrónico</label>
+          <InputComponent
+            type="email"
+            id="email"
+            placeholder="Ingresa tu correo"
+            register={register("email", { required: true })}
+            icon="📧"
+          />
+
+          <label htmlFor="password">Contraseña</label>
+          <InputComponent
+            type="password"
+            id="password"
+            placeholder="Ingresa tu contraseña"
+            register={register("password", { required: true })}
+            icon="👁️"
+          />
+
+          <p className="forgot">¿Olvidaste tu contraseña?</p>
+
+          <ButtonComponent type="submit">Confirmar</ButtonComponent>
+        </form>
+      </div>
+      <div class="decoration-container"></div>
+    </div>
+  );
 };
 
 export default Login;
