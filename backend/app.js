3a30b2845e6f8abe69a04eb0689460930969c59b
Rony <ronyjavierramirezalvarado@gmail.com>
2025-08-06 18:41:59 -0600

    Hice cambios en las rutas del App

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,17 @@
-//Librerias importadas
+// Librerías importadas
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 
-
-//Rutas de verificacion y recuperacion de usuarios
+// Rutas de verificación y recuperación de usuarios
 import registerRoutes from './src/routes/registerClients.js';
-import registerVetRoutes from './src/routes/registerVet.js'
+import registerVetRoutes from './src/routes/registerVet.js';
 import loginRoutes from './src/routes/login.js';
 import logoutRoutes from './src/routes/logout.js';
 import passwordRecovery from './src/routes/passwordRecovery.js';
+import cartRoutes from './src/routes/Cart.js';
 
-import HolidayRoutes from './src/routes/holiday.js';
-import ProductsRoutes from './src/routes/products.js';
-import ReviewsRoutes from './src/routes/reviews.js';
-import CartRoutes from './src/routes/Cart.js';
-import EmpleadosRoutes from "./src/routes/employees.js";
-
-// Rutas de CRUDS
+// Rutas de CRUDs
 import holidayRoutes from './src/routes/holiday.js';
 import clientsRoutes from './src/routes/clients.js';
 import adminRoutes from './src/routes/admin.js';
@@ -25,53 +19,50 @@ import productsRoutes from './src/routes/products.js';
 import reviewsRoutes from './src/routes/reviews.js';
 import employeesRoutes from "./src/routes/employees.js";
 import categoriesRoutes from "./src/routes/categories.js";
-import ordersRoutes from "./src/routes/orders.js"
+import ordersRoutes from "./src/routes/orders.js";
 
-//Rutas Invitados
+// Rutas Invitados
 import guestWholesalers from './src/routes/guestWholesalers.js';
 import guestClients from './src/routes/guestClients.js';
 
-//Rutas Graficas
+// Rutas Gráficas
 import salesRoutes from './src/routes/sales.js';
-
 
 const corsOptions = {
     origin: 'http://localhost:5173',
-    credentials: true, 
-}
+    credentials: true,
+};
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors(corsOptions));
 
-// Rutas verificacion y recuperacion de usuarios
-app.use('/api/register', registerRoutes)
-app.use('/api/login', loginRoutes)
-app.use('/api/logout', logoutRoutes)
-app.use('/api/passwordRecovery', passwordRecovery)
-app.use('/api/registerVet', registerVetRoutes)
+// Rutas de verificación y recuperación de usuarios
+app.use('/api/register', registerRoutes);
+app.use('/api/login', loginRoutes);
+app.use('/api/logout', logoutRoutes);
+app.use('/api/passwordRecovery', passwordRecovery);
+app.use('/api/registerVet', registerVetRoutes);
 
-// Rutas de CRUDS
-app.use('/api/clients', clientsRoutes)
-app.use('/api/employees', employeesRoutes)
-app.use('/api/admin', adminRoutes)
-app.use('/api/holiday', holidayRoutes)
-app.use('/api/products', productsRoutes)
-app.use('/api/orders', ordersRoutes)
-app.use('/api/reviews', reviewsRoutes)
+// Rutas de CRUDs y Graficas
+app.use('/api/clients', clientsRoutes);
+app.use('/api/employees', employeesRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/holiday', holidayRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/orders', ordersRoutes);
+app.use('/api/reviews', reviewsRoutes);
+app.use('/api/categories', categoriesRoutes);
 
-//Rutas Invitados
-app.use('/api/guestWholesalers', guestWholesalers)
-app.use('/api/guestClients', guestClients)
+// Rutas Invitados
+app.use('/api/guestWholesalers', guestWholesalers);
+app.use('/api/guestClients', guestClients);
 
-//Graficas
-app.use('/api/sales', salesRoutes)
-app.use('/api/Holiday', HolidayRoutes)
-app.use('/api/products', ProductsRoutes)
-app.use('/api/reviews', ReviewsRoutes)
-app.use('/api/cart', CartRoutes);
-app.use('/api/employees', EmpleadosRoutes)
-app.use('/api/categories', categoriesRoutes)
+// Rutas Gráficas
+app.use('/api/sales', salesRoutes);
+
+// Carrito
+app.use('/api/cart', cartRoutes);
 
 export default app;
